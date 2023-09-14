a7fa9acc5201074db306a27463377ce69f6ab35f
Zelyna Farrell <zsillas@yahoo.com>
2023-09-14 14:58:00 -0700

    Initial Commit

---
+++ GlassDocker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GlassDocker
+//
+//  Created by Zelyna Sillas on 9/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GlassDocker/GlassDockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassDockerApp.swift
+//  GlassDocker
+//
+//  Created by Zelyna Sillas on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct GlassDockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
